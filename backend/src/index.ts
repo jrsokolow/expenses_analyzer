11a868d0f2ce6aa7fa7ec517254878fa76f61706
Jakub Sokołowski <jrsokolow@gmail.com>
2023-05-23 22:21:07 +0200

    add thrid endpoint to return non matching entries in excel

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,10 @@
-import express, { Request, Response, json } from 'express';
+import express, { Request, Response } from 'express';
 import { Row } from 'read-excel-file';
 
-import path from 'path';
 import readExcelFile from 'read-excel-file/node';
 
 const ALLEGRO = ['Allegro']
-const MARKETS = ['DINO', 'NETTO', 'BIEDRONKA','CARREFOUR'];
+const MARKETS = ['DINO', 'NETTO', 'BIEDRONKA', 'CARREFOUR'];
 const PEPCO = ['PEPCO'];
 const PETROL = ['STACJA PALIW', 'LOTOS', 'ORLEN', 'CIRCLE'];
 const MEDICINE = ['APTEKA'];
@@ -17,8 +16,8 @@ const SMALL_SHOPS = ['ZABKA', 'ZYGULA', 'Piekarnia', 'WIELOBRANZOWY', 'DELIKATES
 const GAMES = ['LONDON', 'GOGcomECOM', 'Google Play', 'Steam', 'STEAM', 'PlayStation'];
 const MEDIA = ['Disney', 'YouTubePremium', 'SKYSHOWTIME'];
 const ORANGE = ['FLEX'];
-const CLOTHS = ['smyk','SECRET', 'SINSAY', 'kappahl', 'MEDICINE', 'HOUSE', 'RESERVED'];
-const CAR_SHOWER = ['WIKON','Myjnia'];
+const CLOTHS = ['smyk', 'SECRET', 'SINSAY', 'kappahl', 'MEDICINE', 'HOUSE', 'RESERVED'];
+const CAR_SHOWER = ['WIKON', 'Myjnia'];
 const FARM = ['ZIELONY ZAKATEK', 'OGRODNICZO'];
 const SHOES = ['CCC'];
 const COSMETICS = ['ROSSMANN'];
@@ -27,12 +26,12 @@ const RESTAURANT = ['SLOW FOOD', 'Verde', 'EWA DA', 'STARA PIEKARNIA', 'MCDONALD
 const MIEDZYZDROJE = ['MIEDZYZDROJE'];
 const CINEMA = ['DOM KULTURY'];
 const SPORT = ['MARTES'];
-const HAIR_CUT = ['FRYZJERSKI','FRYZJERSKA'];
-const PETS = ['PATIVET','KAKADU'];
+const HAIR_CUT = ['FRYZJERSKI', 'FRYZJERSKA'];
+const PETS = ['PATIVET', 'KAKADU'];
 const ENGLISH = ['edoo'];
 
 // Definicja obiektu z mapowaniem stałych
-const constantMap: { [key: string]: string[] } = {
+const constantMap: Record<string, string[]> = {
   ALLEGRO,
   MARKETS,
   PEPCO,
@@ -89,22 +88,17 @@ app.get('/api/data/:constant', async (req: Request, res: Response) => {
 
     const constantArray: string[] = constantMap[constant];
 
-    console.log(constantArray);
-
     const rows: Row[] = await readExcelFile(excelFilePath);
 
     const jsonData: ExcelRow[] = rows
       .slice(1)
-      .filter((row: Row) => { 
-        return isCostMatch(row[6]?.toString() || '', constantArray) 
+      .filter((row: Row) => {
+        return isCostMatch(row[6]?.toString() || '', constantArray)
       })
       .map((row: Row) => ({
         Opis: row[6]?.toString() || '',
         Kwota: row[3]?.toString() || '',
       }));
-
-      console.log(jsonData);
-
 
     // Filtruj dane na podstawie przekazanego parametru
     const filteredData: ExcelRow[] = jsonData.filter((row: ExcelRow) =>
@@ -123,6 +117,37 @@ app.get('/api/data/:constant', async (req: Request, res: Response) => {
   } catch (error) {
     console.error('Wystąpił błąd:', error);
     res.status(500).json({ error: 'Wystąpił błąd serwera.' });
+  }
+});
+
+app.get('/api/constants', (req: Request, res: Response) => {
+  try {
+    res.json(constantMap);
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+app.get('/api/non-matching', async (req: Request, res: Response) => {
+  try {
+    const rows: Row[] = await readExcelFile(excelFilePath);
+
+    const jsonData: ExcelRow[] = rows
+      .slice(1)
+      .map((row: Row) => ({
+        Opis: row[6]?.toString() || '',
+        Kwota: row[3]?.toString() || '',
+      }));
+
+    const nonMatchingRows = jsonData.filter((row: ExcelRow) => {
+      const values = Object.values(constantMap).flat();
+      return !values.some((value) => row.Opis.includes(value));
+    });
+    res.json(nonMatchingRows);
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'Internal server error' });
   }
 });
 
