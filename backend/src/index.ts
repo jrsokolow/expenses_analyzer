55eeb3c09d02ca54871a797d2cb279410050cf9b
Jakub Sokołowski <jrsokolow@gmail.com>
2025-04-22 21:11:27 +0200

    update categoried, add logs

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -13,17 +13,17 @@ const PETROL = ['STACJA PALIW', 'LOTOS', 'ORLEN', 'CIRCLE', 'NOWA SOL MOL'];
 const MEDICINE = ['APTEKA'];
 const DOCTORS = ['MEDICUS', 'ALDEMED', 'PERINATEA'];
 const DENTISTRY = ['STOMATOLOGIA'];
-const DIABETIC = ['diabetyk24', 'FRANCISCO', 'Aero-Medika', 'sugarcubes'];
+const DIABETIC = ['diabetyk24', 'HEROKU', 'Aero-Medika', 'sugarcubes'];
 const TOOLS_SHOPS = ['MROWKA', 'GRANAT'];
 const GAMES = ['GOGcomECOM', 'Steam', 'STEAM', 'PlayStation'];
-const MEDIA = ['YouTubePremium', 'rp.pl', 'Netflix', 'NETFLIX', 'Google Play', 'help.max.com'];
+const MEDIA = ['YouTubePremium', 'rp.pl', 'Netflix', 'NETFLIX', 'Google Play', 'help.max.com', 'YouTube', 'NBA League Pass'];
 const ORANGE = ['FLEX'];
-const CLOTHS = ['zalando', 'miluba.pl', 'smyk', 'SECRET', 'SINSAY', 'kappahl', 'MEDICINE', 'HOUSE', 'RESERVED', 'HM POL', 'GALANTERIA ODZIEZOWA', 'HEBE', 'CROPP'];
+const CLOTHS = ['STRADIVARIUS', 'zalando', 'miluba.pl', 'smyk', 'SECRET', 'SINSAY', 'kappahl', 'MEDICINE', 'HOUSE', 'RESERVED', 'HM POL', 'GALANTERIA ODZIEZOWA', 'HEBE', 'CROPP', 'vinted'];
 const CAR_SHOWER = ['WIKON', 'Myjnia'];
 const SHOES = ['CCC', 'e-cizemka', 'ccc.eu', 'eobuwie', 'zapato'];
 const COSMETICS = ['ROSSMANN', 'SZALATA CHLEBOWSKA'];
 const EMPIK = ['EMPIK'];
-const RESTAURANT = ['GRYCAN LODY OD POKOLEN', 'TOMASZ KUROS', 'ZIELONA GORA BW SPOLKA Z O.O.', 'MOCCA', 'KARMEL', 'SLOW FOOD', 'Verde', 'EWA DA', 'STARA PIEKARNIA', 'MCDONALDS', 'TCHIBO', 'PIJALNIA KAWY I CZEKO', 'KUCHNIE SWIATA', 'HEBAN', 'Ohy', 'KRATKA', 'Wafelek i Kulka', 'CIACHOO', 'PIERINO', 'CAFFETTERIA GELATERIA'];
+const RESTAURANT = ['PIZZERIA LUCA', 'STACJA CAFE', 'CAFE SAN-REMO', 'GRYCAN LODY OD POKOLEN', 'TOMASZ KUROS', 'ZIELONA GORA BW SPOLKA Z O.O.', 'MOCCA', 'KARMEL', 'SLOW FOOD', 'Verde', 'EWA DA', 'STARA PIEKARNIA', 'MCDONALDS', 'TCHIBO', 'PIJALNIA KAWY I CZEKO', 'KUCHNIE SWIATA', 'HEBAN', 'Ohy', 'KRATKA', 'Wafelek i Kulka', 'CIACHOO', 'PIERINO', 'CAFFETTERIA GELATERIA'];
 const MIEDZYZDROJE = ['MIEDZYZDROJE'];
 const CINEMA = ['DOM KULTURY', 'cinema-city'];
 const SPORT = ['www.decathlon.pl', 'MARTES'];
@@ -108,10 +108,12 @@ app.get('/api/costs', async (req: Request, res: Response) => {
     const costs: Record<string, number> = {};
 
     Object.keys(constantMap).forEach(constant => {
+      console.log(constant);
       const constantArray = constantMap[constant];
       const totalAmount = jsonData.reduce(
         (sum: number, row: ExcelRow) => {
           if (isCostMatch(row.Opis, constantArray)) {
+            console.log(row.Opis + ' ' + row.Kwota);
             return sum + parseFloat(row.Kwota.replace(',', '').replace('-', ''));
           }
           return sum;
@@ -119,6 +121,7 @@ app.get('/api/costs', async (req: Request, res: Response) => {
         0
       );
       costs[constant] = Math.floor(totalAmount);
+      console.log('>>>>>>>>>>>>>>>>>>');
     });
 
     res.json(costs);
